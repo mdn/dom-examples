41b4a453321057ef4ed77e063b33749a3169f219
Chris Mills <cmills@opera.com>
2013-10-22 13:03:16 +0100

    fixing Chromes non-support of navigator.mozApps by wrapping a feature detect around all of install.js

--- scripts/install.js
+++ scripts/install.js
@@ -1,29 +1,32 @@
-// define the manifest URL
-var manifest_url = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
-
-function install(ev) {
-  ev.preventDefault();
-  // install the app
-  var installLocFind = navigator.mozApps.install(manifest_url);
-  installLocFind.onsuccess = function(data) {
-    // App is installed, do something if you like
-  };
-  installLocFind.onerror = function() {
-    // App wasn't installed, info is in
-    // installapp.error.name
-    alert(installLocFind.error.name);
-  };
-};
-
-// get a reference to the button and call install() on click
-var button = document.getElementById('install');
-
-var installCheck = navigator.mozApps.checkInstalled(manifest_url);
-installCheck.onsuccess = function() {
-  
-  if(installCheck.result) {
-    button.style.display = "none";
-  } else {
-    button.addEventListener('click', install, false);
-  };
-};
+if('mozApps' in navigator) {
+    
+    // define the manifest URL
+    var manifest_url = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+    
+    function install(ev) {
+      ev.preventDefault();
+      // install the app
+      var installLocFind = navigator.mozApps.install(manifest_url);
+      installLocFind.onsuccess = function(data) {
+        // App is installed, do something if you like
+      };
+      installLocFind.onerror = function() {
+        // App wasn't installed, info is in
+        // installapp.error.name
+        alert(installLocFind.error.name);
+      };
+    };
+    
+    // get a reference to the button and call install() on click
+    var button = document.getElementById('install');
+    
+    var installCheck = navigator.mozApps.checkInstalled(manifest_url);
+    installCheck.onsuccess = function() {
+      
+      if(installCheck.result) {
+        button.style.display = "none";
+      } else {
+        button.addEventListener('click', install, false);
+      };
+    };
+}
