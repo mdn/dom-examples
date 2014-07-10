8b0b99b0e249b640d313723bc5fd527e1ef1cb49
Chris Mills <cmills@opera.com>
2014-07-10 15:15:41 +0100

    removing unneeded files

--- serviceworker.js
+++
@@ -1,9 +0,0 @@
-// scope defaults to "/*"
-navigator.serviceWorker.register("worker.js").then(
-  function(worker) {
-    console.log("success!");
-    serviceWorker.postMessage("Howdy from your installing page.");
-    // To use the serviceWorker immediately, you might call window.location.reload()
-  }).catch(function(e) {
-    console.error("Installing the worker failed");
-  });
