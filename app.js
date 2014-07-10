699cdee4ecc7ba64452b03f1e716b86dfec17f15
Chris Mills <cmills@opera.com>
2014-07-10 15:51:32 +0100

    test

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js', {
     scope: '/*'
   }).then(function(sw) {
-    // registration worked!
+    // registration worked
     console.log('Registration succeeded.');
   }).catch(function() {
-    // registration failed :(
+    // registration failed
     console.log('Registration failed.');
   });
 }
