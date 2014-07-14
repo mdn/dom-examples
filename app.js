43f4a6b3d5bc53194957d8f572dacce588315ce5
Chris Mills <cmills@opera.com>
2014-07-14 19:21:15 +0100

    updating to proper example; adding image gallery, imgLoad promises function to load images, code to build display

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// register service worker
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw-test/sw.js', {
     scope: '/*'
@@ -9,3 +11,57 @@ if ('serviceWorker' in navigator) {
     console.log('Registration failed.');
   });
 }
+
+// function for loading each image via XHR
+
+function imgLoad(url) {
+// return a promise for an image loading
+return new Promise(function(resolve, reject) {	  
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.responseType = 'blob';
+  
+  request.onload = function() {
+	    if (request.status == 200) {
+	      resolve(request.response);
+	    } else {
+	      reject(Error('Image didn\'t load successfully; error code:' + request.statusText));
+	    }
+  };
+
+    request.onerror = function() {
+      reject(Error('There was a network error.'));
+  };
+  
+  // Send the request
+  request.send();
+  });
+};
+
+// load each set of image, alt text and caption
+
+var imgJSON = Gallery.images;
+var imgSection = document.querySelector('section');
+
+window.onload = function() {
+  for(i = 0; i<imgJSON.length; i++) {
+    imgLoad(imgJSON[i].url).then(function(response) {
+
+      var myImage = document.CreateElement('img');
+      var myFigure = document.CreateElement('figure');
+      var myCaption = document.CreateElement('caption');
+
+      var imageURL = window.URL.createObjectURL(response);
+	  myImage.src = imageURL;
+      myImage.setAttribute('alt', imgJSON[i].alt);
+      myCaption.innerHTML = imgJSON[i].name + ': Taken by ' + imgJSON[i].credit;
+
+      imgSection.appendChild(myFigure);
+      myFigure.appendChild(myImage);
+      myFigure.appendChild(myCaption);
+
+	}, function(Error) {
+	  console.log(Error);
+	});
+  }
+};
