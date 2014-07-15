0334eb7195442c2862b9b81b6845a2616136ab57
Chris Mills <cmills@opera.com>
2014-07-15 15:51:51 +0100

    debugging caching issues

--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.create('v1').then(function(cache) {
       return cache.add(
+        '/sw-test/',
         '/sw-test/index.html',
         '/sw-test/style.css',
         '/sw-test/app.js',
         '/sw-test/image-list.js',
         '/sw-test/star-wars-logo.jpg',
+        '/sw-test/gallery/',
         '/sw-test/gallery/*'
       );
-
-      console.log('Caching succeeded.');
     });
   );
 });
