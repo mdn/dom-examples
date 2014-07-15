aeaabbe10eb08da014816130ab173f92f7200200
Chris Mills <cmills@opera.com>
2014-07-15 10:49:52 +0100

    serviceworker install event

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.create('v1').then(function(cache) {
+      return cache.add(
+        '/sw-test/index.html',
+        '/sw-test/style.css',
+        '/sw-test/app.js',
+        '/sw-test/image-list.js',
+        '/sw-test/star-wars-logo.jpg',
+        '/sw-test/gallery/*',
+      );
+    })
+  );
+});
