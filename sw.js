c195e883c01befd864986c5eaf8bfef09973a39e
Chris Mills <cmills@opera.com>
2014-07-16 14:33:03 +0100

    trying other install event code to see if I have more luck getting service worker to work

--- sw.js
+++ sw.js
@@ -1,41 +1,44 @@
-this.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.create('v1').then(function(cache) {
-      return cache.add(
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
-      );
-    })
-  );
-});
+// this.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.create('v1').then(function(cache) {
+//       return cache.add(
+//         '/sw-test/',
+//         '/sw-test/index.html',
+//         '/sw-test/style.css',
+//         '/sw-test/app.js',
+//         '/sw-test/image-list.js',
+//         '/sw-test/star-wars-logo.jpg',
+//         '/sw-test/gallery/',
+//         '/sw-test/gallery/bountyHunters.jpg',
+//         '/sw-test/gallery/myLittleVader.jpg',
+//         '/sw-test/gallery/snowTroopers.jpg'
+//       );
+//     })
+//   );
+// });
 
 // You could also do
 
-// this.addEventListener('install', function(event) {
-//   var starWarsAssets = new Cache();
+this.addEventListener('install', function(event) {
+  var starWarsAssets = new Cache();
 
-//   event.waitUntil(
-//     starWarsAssets.add(
-//       '/sw-test/index.html',
-//       '/sw-test/style.css',
-//       '/sw-test/app.js',
-//       '/sw-test/image-list.js',
-//       '/sw-test/star-wars-logo.jpg',
-//       '/sw-test/gallery/*'
-//     )
-//   )
+  event.waitUntil(
+    starWarsAssets.add(
+      '/sw-test/index.html',
+      '/sw-test/style.css',
+      '/sw-test/app.js',
+      '/sw-test/image-list.js',
+      '/sw-test/star-wars-logo.jpg',
+      '/sw-test/gallery/',
+      '/sw-test/gallery/bountyHunters.jpg',
+      '/sw-test/gallery/myLittleVader.jpg',
+      '/sw-test/gallery/snowTroopers.jpg'
+    )
+  )
 
-//   caches.set('v1', starWarsResources);
+  caches.set('v1', starWarsResources);
 
-// )};
+)};
 
 this.addEventListener('fetch', function(event) {
   event.respondWith(
