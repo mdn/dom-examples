fb9c211e83902f39beb6a5f6a88a052b2fbf0779
Chris Mills <cmills@opera.com>
2014-09-17 21:46:42 +0100

    updating fetch event handlerblock in service worker to be less confusing, there were two return statements before

--- sw.js
+++ sw.js
@@ -43,12 +43,14 @@ this.addEventListener('install', function(event) {
 this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).catch(function() {
-      return event.default();
-      return caches.get('v1').then(function(cache) {
-        cache.add(event.request);
+      return event.default().then(function(response) {
+        caches.get('v1').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });  
       });
     }).catch(function() {
-      return caches.match('/sw-test/gallery/myLittleVader.jpg');
-    })
+     return caches.match('/sw-test/gallery/myLittleVader.jpg');
+    });
   );
 });
