eaf22707c871ed9ed32558157c12a91bada6776b
Chris Mills <cmills@opera.com>
2014-09-18 06:58:17 +0100

    adding error printout to register failure callback to get more information on why registration is failing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw-test/sw.js', {
-    scope: '/*'
+    scope: '/'
   }).then(function() {
     // registration worked
     console.log('Registration succeeded.');
