180f52a2462fd3d04357dbe120aa3e63fcdf2593
Chris Mills <cmills@opera.com>
2014-09-26 14:53:01 +0100

    added cursor.advance() example, and tidied up the code a bit

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,25 @@
 var db;
 
 var records = [
-      { albumTitle: "Power windows", year: 1985 },
-      { albumTitle: "Grace under pressure", year: 1984 },
-      { albumTitle: "Signals", year: 1982 },
-      { albumTitle: "Moving pictures", year: 1981 },
-      { albumTitle: "Permanent Waves", year: 1980 },
-      { albumTitle: "Hemispheres", year: 1978 },
-      { albumTitle: "A farewell to kings", year: 1977 },
-      { albumTitle: "2112", year: 1976 },
-      { albumTitle: "Caress of steel", year: 1975 },
-      { albumTitle: "Fly by night", year: 1975 },
-      { albumTitle: "Rush", year: 1974 }
+      { albumTitle: 'Power windows', year: 1985 },
+      { albumTitle: 'Grace under pressure', year: 1984 },
+      { albumTitle: 'Signals', year: 1982 },
+      { albumTitle: 'Moving pictures', year: 1981 },
+      { albumTitle: 'Permanent Waves', year: 1980 },
+      { albumTitle: 'Hemispheres', year: 1978 },
+      { albumTitle: 'A farewell to kings', year: 1977 },
+      { albumTitle: '2112', year: 1976 },
+      { albumTitle: 'Caress of steel', year: 1975 },
+      { albumTitle: 'Fly by night', year: 1975 },
+      { albumTitle: 'Rush', year: 1974 }
     ];
 
 // all the variables we need for the app    
 
 var list = document.querySelector('ul');
-var pre = document.querySelector('pre');
+var advance = document.querySelector('.advance');
+var useContinue = document.querySelector('.continue');
+
 
 window.onload = function() {
   // In the following line, you should include the prefixes of implementations you want to test.
@@ -29,7 +31,7 @@ window.onload = function() {
   window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
   // (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
 
-  var DBOpenRequest = window.indexedDB.open("albumLists", 1);
+  var DBOpenRequest = window.indexedDB.open('albumLists', 1);
   
   DBOpenRequest.onsuccess = function(event) {
     db = DBOpenRequest.result;
@@ -43,14 +45,12 @@ window.onload = function() {
       note.innerHTML += '<li>Error loading database.</li>';
     };
     
-    var objectStore = db.createObjectStore("rushAlbumList", { keyPath: "albumTitle" });
-    
-    objectStore.createIndex("year", "year", { unique: false });
-    objectStore.createIndex("coverImage", "coverImage", { unique: true });
+    var objectStore = db.createObjectStore('rushAlbumList', { keyPath: 'albumTitle' });  
+    objectStore.createIndex('year', 'year', { unique: false });
   };
     
   function populateData() {
-    var transaction = db.transaction(['rushAlbumList'], "readwrite");
+    var transaction = db.transaction(['rushAlbumList'], 'readwrite');
     var objectStore = transaction.objectStore('rushAlbumList');
     for(i = 0; i < records.length ; i++) {
       var request = objectStore.put(records[i]);
@@ -61,8 +61,13 @@ window.onload = function() {
     };
   };
 
+  useContinue.onclick = function() {
+    displayData();
+  }
+
   function displayData() {
-    var transaction = db.transaction(['rushAlbumList'], "readonly");
+    list.innerHTML = '';
+    var transaction = db.transaction(['rushAlbumList'], 'readonly');
     var objectStore = transaction.objectStore('rushAlbumList');
 
     objectStore.openCursor().onsuccess = function(event) {
@@ -75,9 +80,34 @@ window.onload = function() {
           cursor.continue();
         } else {
           console.log('Entries all displayed.');
+          
         }
     };
 
   };
+
+  advance.onclick = function() {
+      advanceResult();
+  };
+
+  function advanceResult() {
+    list.innerHTML = '';
+    var transaction = db.transaction(['rushAlbumList'], 'readonly');
+    var objectStore = transaction.objectStore('rushAlbumList');
+
+    objectStore.openCursor().onsuccess = function(event) {
+      var cursor = event.target.result;
+      if(cursor) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = '<strong>' + cursor.value.albumTitle + '</strong>, ' + cursor.value.year;
+        list.appendChild(listItem);  
+        cursor.advance(2);         
+      } else {
+        console.log('Every other entry displayed.');         
+      }
+    };
+  };
+
+
 };
 
