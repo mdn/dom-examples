61ee9bf8f72872aa40bb4f7eaf1a6fa2b3e82208
Chris Mills <cmills@opera.com>
2014-12-05 09:06:12 -0800

    initial repo population

---
+++ main.js
@@ -0,0 +1,39 @@
+var htmlElem = document.querySelector('html');
+var pElem = document.querySelector('p');
+var imgElem = document.querySelector('img');
+
+var bgcolorForm = document.getElementById('bgcolor');
+var fontForm = document.getElementById('font');
+var imageForm = document.getElementById('image');
+
+if(!localStorage.getItem('bgcolor')) {
+  populateStorage();
+} else {
+  setStyles();
+}
+
+function populateStorage() {
+  localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
+  localStorage.setItem('font', document.getElementById('font').value);
+  localStorage.setItem('image', document.getElementById('image').value);
+
+  setStyles();
+}
+
+function setStyles() {
+  var currentColor = localStorage.getItem('bgcolor');
+  var currentFont = localStorage.getItem('font');
+  var currentImage = localStorage.getItem('image');
+
+  document.getElementById('bgcolor').value = currentColor;
+  document.getElementById('font').value = currentFont;
+  document.getElementById('image').value = currentImage;
+
+  htmlElem.style.backgroundColor = '#' + currentColor;
+  pElem.style.fontFamily = currentFont;
+  imgElem.setAttribute('src', currentImage);
+}
+
+bgcolorForm.onchange = populateStorage;
+fontForm.onchange = populateStorage;
+imageForm.onchange = populateStorage;
