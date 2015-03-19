931d9eb151b9116086b1d47a4c18c027ca020e4e
chrisdavidmills <chrisdavidmills@gmail.com>
2015-03-19 11:30:45 +0000

    updating SW code to account for recent spec changes. changing create to open, replacing event.default with fetch(event.request), and changing add to addAll

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', {
+  navigator.serviceWorker.register('/sw.js', {
     scope: '/'
-  }).then(function() {
+  }).then(function(reg) {
     // registration worked
-    console.log('Registration succeeded.');
+    console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
     // registration failed
     console.log('Registration failed with ' + error);
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.create('v1').then(function(cache) {
-      return cache.add(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(
         '/sw-test/',
         '/sw-test/index.html',
         '/sw-test/style.css',
@@ -17,33 +17,10 @@ this.addEventListener('install', function(event) {
   );
 });
 
-// You could also do
-
-// this.addEventListener('install', function(event) {
-//   var starWarsAssets = new Cache();
-
-//   event.waitUntil(
-//     starWarsAssets.add(
-//       '/sw-test/index.html',
-//       '/sw-test/style.css',
-//       '/sw-test/app.js',
-//       '/sw-test/image-list.js',
-//       '/sw-test/star-wars-logo.jpg',
-//       '/sw-test/gallery/',
-//       '/sw-test/gallery/bountyHunters.jpg',
-//       '/sw-test/gallery/myLittleVader.jpg',
-//       '/sw-test/gallery/snowTroopers.jpg'
-//     )
-//   )
-
-//   caches.set('v1', starWarsResources);
-
-// )};
-
 this.addEventListener('fetch', function(event) {
   var cachedResponse = caches.match(event.request).catch(function() {
-    return event.default().then(function(response) {
-      return caches.get('v1').then(function(cache) {
+    return fetch(event.request).then(function(response) {
+      return caches.open('v1').then(function(cache) {
         cache.put(event.request, response.clone());
         return response;
       });  
