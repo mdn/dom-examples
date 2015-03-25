301c3f0ae79c415e31d0e81ae3b4942b5eb74520
chrisdavidmills <chrisdavidmills@gmail.com>
2015-03-25 10:38:23 +0000

    replacing SW caching logic with a better propsal, as suggested by Ben Kelly

--- sw.js
+++ sw.js
@@ -17,18 +17,15 @@ this.addEventListener('install', function(event) {
   );
 });
 
-this.addEventListener('fetch', function(event) {
-  var cachedResponse = caches.match(event.request).catch(function() {
-    return fetch(event.request).then(function(response) {
-      return caches.open('v1').then(function(cache) {
-        cache.put(event.request, response.clone());
-        return response;
-      });  
-    });
-  }).catch(function() {
-    return caches.match('/sw-test/gallery/myLittleVader.jpg');
-  });
-    
-  event.respondWith(cachedResponse);
+var response;
+var cachedResponse = caches.match(event.request).catch(function() {
+  return fetch(event.request);
+}).then(function(r) {
+  response = r;
+  caches.open('v1').then(function(cache) {
+    cache.put(event.request, response);
+  });  
+  return response.clone();
+}).catch(function() {
+  return caches.match('/sw-test/gallery/myLittleVader.jpg');
 });
-
