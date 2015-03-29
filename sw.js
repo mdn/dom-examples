9bf8c00482b254e5a3a96dc4356803d5e74b0043
chrisdavidmills <chrisdavidmills@gmail.com>
2015-03-29 18:10:51 +0100

    adding event listener block to fix broken sw!

--- sw.js
+++ sw.js
@@ -17,15 +17,17 @@ this.addEventListener('install', function(event) {
   );
 });
 
-var response;
-var cachedResponse = caches.match(event.request).catch(function() {
-  return fetch(event.request);
-}).then(function(r) {
-  response = r;
-  caches.open('v1').then(function(cache) {
-    cache.put(event.request, response);
-  });  
-  return response.clone();
-}).catch(function() {
-  return caches.match('/sw-test/gallery/myLittleVader.jpg');
+this.addEventListener('fetch', function(event) {
+  var response;
+  var cachedResponse = caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });  
+    return response.clone();
+  }).catch(function() {
+    return caches.match('/sw-test/gallery/myLittleVader.jpg');
+  });
 });
