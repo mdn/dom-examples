b67d150b3e7b2161cfa98e94e0cb57a81bf4127d
chrisdavidmills <chrisdavidmills@gmail.com>
2015-05-27 20:22:25 +0100

    adding console logging for registration state

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
     // registration worked
     console.log('Registration succeeded. Scope is ' + reg.scope);
+    
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+    
   }).catch(function(error) {
     // registration failed
     console.log('Registration failed with ' + error);
