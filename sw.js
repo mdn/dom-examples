b273dd9e88d6ca175ec9fc283a9d0470bb03f065
chrisdavidmills <chrisdavidmills@gmail.com>
2015-06-17 14:52:13 +0100

    testing Caches.open block

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll(
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
-      );
-    })
+    // caches.open('v1').then(function(cache) {
+    //   return cache.addAll(
+    //     '/sw-test/',
+    //     '/sw-test/index.html',
+    //     '/sw-test/style.css',
+    //     '/sw-test/app.js',
+    //     '/sw-test/image-list.js',
+    //     '/sw-test/star-wars-logo.jpg',
+    //     '/sw-test/gallery/',
+    //     '/sw-test/gallery/bountyHunters.jpg',
+    //     '/sw-test/gallery/myLittleVader.jpg',
+    //     '/sw-test/gallery/snowTroopers.jpg'
+    //   );
+    // })
   );
 });
 
