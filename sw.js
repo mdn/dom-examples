56392ebbf220b606b18e9a1af0a5ea01e31ef8bd
chrisdavidmills <chrisdavidmills@gmail.com>
2015-06-17 15:02:09 +0100

    adding onstatechange block to test sate change

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 this.addEventListener('install', function(event) {
-  // event.waitUntil(
-  //   caches.open('v1').then(function(cache) {
-  //     return cache.addAll(
-  //       '/sw-test/',
-  //       '/sw-test/index.html',
-  //       '/sw-test/style.css',
-  //       '/sw-test/app.js',
-  //       '/sw-test/image-list.js',
-  //       '/sw-test/star-wars-logo.jpg',
-  //       '/sw-test/gallery/',
-  //       '/sw-test/gallery/bountyHunters.jpg',
-  //       '/sw-test/gallery/myLittleVader.jpg',
-  //       '/sw-test/gallery/snowTroopers.jpg'
-  //     );
-  //   })
-  // );
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(
+        '/sw-test/',
+        '/sw-test/index.html',
+        '/sw-test/style.css',
+        '/sw-test/app.js',
+        '/sw-test/image-list.js',
+        '/sw-test/star-wars-logo.jpg',
+        '/sw-test/gallery/',
+        '/sw-test/gallery/bountyHunters.jpg',
+        '/sw-test/gallery/myLittleVader.jpg',
+        '/sw-test/gallery/snowTroopers.jpg'
+      );
+    })
+  );
 });
 
 this.addEventListener('fetch', function(event) {
