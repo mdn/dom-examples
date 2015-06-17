6c70cf6bda7392d1651ab8e49a78d9a7542b7041
chrisdavidmills <chrisdavidmills@gmail.com>
2015-06-17 15:08:51 +0100

    trying to debug put problem in sw

--- sw.js
+++ sw.js
@@ -21,8 +21,7 @@ this.addEventListener('fetch', function(event) {
   var response;
   var cachedResponse = caches.match(event.request).catch(function() {
     return fetch(event.request);
-  }).then(function(r) {
-    response = r;
+  }).then(function(response) {
     caches.open('v1').then(function(cache) {
       cache.put(event.request, response);
     });  
