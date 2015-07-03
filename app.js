62c4ac0d416632a8bb3d737e2b7a69a5cd93e04c
chrisdavidmills <chrisdavidmills@gmail.com>
2015-07-03 08:34:31 +0100

    reverting

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/' }).then(function(reg) {
+  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
     
     if(reg.installing) {
       console.log('Service worker installing');
