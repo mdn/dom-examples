c962f54573c0a5944fa22ae1048d9ecfcda34927
chrisdavidmills <chrisdavidmills@gmail.com>
2015-07-03 08:38:55 +0100

    installation test

--- sw.js
+++ sw.js
@@ -1,18 +1,7 @@
 this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
-      ]);
+      return cache.add('/sw-test/index.html');
     })
   );
 });
