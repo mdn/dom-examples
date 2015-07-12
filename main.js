a0ed1a9e072fc254f5eb4add826aa6e1ab6ad68c
lokeshjain2008 <lokeshjainajay@yahoo.com>
2015-07-12 20:51:23 +0530

    Added some comment to make it clear
    Thanks for the tutorials you created. This may be very little change but it answers some questions for a newbie like me.

--- main.js
+++ main.js
@@ -3,21 +3,22 @@ var second = document.querySelector('#number2');
 
 var result = document.querySelector('.result');
 
-if (!!window.Worker) {
+if (window.Worker) { //check if Browser supports the Worker api.
+	// Requries script name as input
 	var myWorker = new Worker("worker.js");
 
 	first.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]);
+	  myWorker.postMessage([first.value,second.value]); //seding message as array to the worker
 	  console.log('Message posted to worker');
-	}
+	};
 
 	second.onchange = function() {
 	  myWorker.postMessage([first.value,second.value]);
 	  console.log('Message posted to worker');
-	}
+	};
 
 	myWorker.onmessage = function(e) {
 		result.textContent = e.data;
 		console.log('Message received from worker');
-	}
+	};
 }
