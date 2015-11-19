cd92f33060da9e4de416a545f40894eebdbb00dc
chrisdavidmills <chrisdavidmills@gmail.com>
2015-11-19 16:10:24 +0000

    adding comment to explain how the result is grabbed from the onresult event object

--- speech-color-changer/script.js
+++ speech-color-changer/script.js
@@ -17,6 +17,14 @@ document.body.ontouchend = function() {
 }
 
 recognition.onresult = function(event) {
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The first [0] is returning the SpeechRecognitionResult at position 0.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The second [0] is returning the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object 
   var color = event.results[0][0].transcript;
   diagnostic.textContent = 'Result received: ' + color;
   bg.style.backgroundColor = color;
