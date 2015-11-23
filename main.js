21d39786dcb74147b88d3124151c0a434e0a063d
chrisdavidmills <chrisdavidmills@gmail.com>
2015-11-23 12:31:05 +0000

    adding comment to mention that onkeyup could be used instead of onchange

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ var second = document.querySelector('#number2');
 
 var result = document.querySelector('.result');
 
-if (window.Worker) { //check if Browser supports the Worker api.
+if (window.Worker) { // Check if Browser supports the Worker api.
 	// Requries script name as input
 	var myWorker = new Worker("worker.js");
 
+// onkeyup could be used instead of onchange if you wanted to update the answer every time
+// an entered value is changed, and you don't want to have to unfocus the field to update its .value
+
 	first.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]); //seding message as array to the worker
+	  myWorker.postMessage([first.value,second.value]); // Sending message as an array to the worker
 	  console.log('Message posted to worker');
 	};
 
