3101f2e9840e5416bdd36a78d15ac3a18a446e36
chrisdavidmills <chrisdavidmills@gmail.com>
2015-11-25 17:55:53 +0000

    adding confidence output

--- speech-color-changer/script.js
+++ speech-color-changer/script.js
@@ -28,6 +28,7 @@ recognition.onresult = function(event) {
   var color = event.results[0][0].transcript;
   diagnostic.textContent = 'Result received: ' + color + '.';
   bg.style.backgroundColor = color;
+  console.log('Confidence: ' + event.results[0][0].confidence);
 }
 
 recognition.onspeechend = function() {
