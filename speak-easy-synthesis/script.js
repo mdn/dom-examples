6c4948e3178ff9ccde0c87ee4cee6ea1da92f846
chrisdavidmills <chrisdavidmills@gmail.com>
2015-11-27 16:24:00 +0000

    adding code to synthesis demo to detect and highlight default voice

--- speak-easy-synthesis/script.js
+++ speak-easy-synthesis/script.js
@@ -15,6 +15,11 @@ var voices = synth.getVoices();
 for(i = 0; i < voices.length ; i++) {
   var option = document.createElement('option');
   option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+  
+  if(voices[i].default) {
+    option.textContent += ' -- DEFAULT';
+  }
+
   option.setAttribute('data-lang', voices[i].lang);
   option.setAttribute('data-name', voices[i].name);
   voiceSelect.appendChild(option);
