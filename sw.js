a0b0b3605a648f718bffd201116acd47451401d2
Anov Siradj <anov.siradj22@gmail.com>
2017-05-10 13:32:51 +0700

    remove `response = r` sw.js

--- sw.js
+++ sw.js
@@ -17,11 +17,9 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-  var response;
   event.respondWith(caches.match(event.request).catch(function() {
     return fetch(event.request);
-  }).then(function(r) {
-    response = r;
+  }).then(function(response) {
     caches.open('v1').then(function(cache) {
       cache.put(event.request, response);
     });
