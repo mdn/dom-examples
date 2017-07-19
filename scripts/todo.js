27acc16befcb389e57090543ac72bdc20a8f0e84
chrisdavidmills <chrisdavidmills@gmail.com>
2017-07-19 15:09:05 +0100

    fixing confusing messages concerning success and complete

--- scripts/todo.js
+++ scripts/todo.js
@@ -10,7 +10,7 @@ var newItem = [
       { taskTitle: "", hours: 0, minutes: 0, day: 0, month: "", year: 0, notified: "no" }
     ];
 
-// all the variables we need for the app    
+// all the variables we need for the app
 var taskList = document.getElementById('task-list');
 
 var taskForm = document.getElementById('task-form');
@@ -37,7 +37,7 @@ window.onload = function() {
 
   // Let us open our database
   var DBOpenRequest = window.indexedDB.open("toDoList", 4);
-   
+
   // Gecko-only IndexedDB temp storage option:
   // var request = window.indexedDB.open("toDoList", {version: 4, storage: "temporary"});
 
@@ -45,34 +45,34 @@ window.onload = function() {
   DBOpenRequest.onerror = function(event) {
     note.innerHTML += '<li>Error loading database.</li>';
   };
-  
+
   DBOpenRequest.onsuccess = function(event) {
     note.innerHTML += '<li>Database initialised.</li>';
-    
+
     // store the result of opening the database in the db variable. This is used a lot below
     db = DBOpenRequest.result;
-    
+
     // Run the displayData() function to populate the task list with all the to-do list data already in the IDB
     displayData();
   };
-  
+
   // This event handles the event whereby a new version of the database needs to be created
   // Either one has not been created before, or a new version number has been submitted via the
   // window.indexedDB.open line above
   //it is only implemented in recent browsers
-  DBOpenRequest.onupgradeneeded = function(event) { 
+  DBOpenRequest.onupgradeneeded = function(event) {
     var db = event.target.result;
-    
+
     db.onerror = function(event) {
       note.innerHTML += '<li>Error loading database.</li>';
     };
 
     // Create an objectStore for this database
-    
+
     var objectStore = db.createObjectStore("toDoList", { keyPath: "taskTitle" });
-    
+
     // define what data items the objectStore will contain
-    
+
     objectStore.createIndex("hours", "hours", { unique: false });
     objectStore.createIndex("minutes", "minutes", { unique: false });
     objectStore.createIndex("day", "day", { unique: false });
@@ -80,15 +80,15 @@ window.onload = function() {
     objectStore.createIndex("year", "year", { unique: false });
 
     objectStore.createIndex("notified", "notified", { unique: false });
-    
+
     note.innerHTML += '<li>Object store created.</li>';
   };
-    
+
   function displayData() {
     // first clear the content of the task list so that you don't get a huge long list of duplicate stuff each time
     //the display is updated.
     taskList.innerHTML = "";
-  
+
     // Open our object store and then get a cursor list of all the different data items in the IDB to iterate through
     var objectStore = db.transaction('toDoList').objectStore('toDoList');
     objectStore.openCursor().onsuccess = function(event) {
@@ -97,7 +97,7 @@ window.onload = function() {
         if(cursor) {
           // create a list item to put each data item inside when displaying it
           var listItem = document.createElement('li');
-          
+
           // check which suffix the deadline day of the month needs
           if(cursor.value.day == 1 || cursor.value.day == 21 || cursor.value.day == 31) {
             daySuffix = "st";
@@ -106,55 +106,55 @@ window.onload = function() {
           } else if(cursor.value.day == 3 || cursor.value.day == 23) {
             daySuffix = "rd";
           } else {
-            daySuffix = "th";  
+            daySuffix = "th";
           }
-          
+
           // build the to-do list entry and put it into the list item via innerHTML.
           listItem.innerHTML = cursor.value.taskTitle + ' — ' + cursor.value.hours + ':' + cursor.value.minutes + ', ' + cursor.value.month + ' ' + cursor.value.day + daySuffix + ' ' + cursor.value.year + '.';
-          
+
           if(cursor.value.notified == "yes") {
             listItem.style.textDecoration = "line-through";
             listItem.style.color = "rgba(255,0,0,0.5)";
           }
 
           // put the item item inside the task list
-          taskList.appendChild(listItem);  
-          
+          taskList.appendChild(listItem);
+
           // create a delete button inside each list item, giving it an event handler so that it runs the deleteButton()
           // function when clicked
           var deleteButton = document.createElement('button');
           listItem.appendChild(deleteButton);
           deleteButton.innerHTML = 'X';
-          // here we are setting a data attribute on our delete button to say what task we want deleted if it is clicked! 
+          // here we are setting a data attribute on our delete button to say what task we want deleted if it is clicked!
           deleteButton.setAttribute('data-task', cursor.value.taskTitle);
           deleteButton.onclick = function(event) {
             deleteItem(event);
           }
-          
+
           // continue on to the next item in the cursor
           cursor.continue();
-        
-        // if there are no more cursor items to iterate through, say so, and exit the function 
+
+        // if there are no more cursor items to iterate through, say so, and exit the function
         } else {
           note.innerHTML += '<li>Entries all displayed.</li>';
         }
       }
     }
-  
+
   // give the form submit button an event listener so that when the form is submitted the addData() function is run
   taskForm.addEventListener('submit',addData,false);
-  
+
   function addData(e) {
     // prevent default - we don't want the form to submit in the conventional way
     e.preventDefault();
-    
+
     // Stop the form submitting if any values are left empty. This is just for browsers that don't support the HTML5 form
     // required attributes
     if(title.value == '' || hours.value == null || minutes.value == null || day.value == '' || month.value == '' || year.value == null) {
       note.innerHTML += '<li>Data not submitted — form incomplete.</li>';
       return;
     } else {
-      
+
       // grab the values entered into the form fields and store them in an object ready for being inserted into the IDB
       var newItem = [
         { taskTitle: title.value, hours: hours.value, minutes: minutes.value, day: day.value, month: month.value, year: year.value, notified: "no" }
@@ -162,13 +162,13 @@ window.onload = function() {
 
       // open a read/write db transaction, ready for adding the data
       var transaction = db.transaction(["toDoList"], "readwrite");
-    
-      // report on the success of opening the transaction
+
+      // report on the success of the transaction completing, when everything is done
       transaction.oncomplete = function() {
         note.innerHTML += '<li>Transaction completed: database modification finished.</li>';
 
         // update the display of data to show the newly added item, by running displayData() again.
-        displayData(); 
+        displayData();
       };
 
       transaction.onerror = function() {
@@ -183,13 +183,15 @@ window.onload = function() {
       console.log(objectStore.transaction);
       console.log(objectStore.autoIncrement);
 
-      // add our newItem object to the object store
-      var objectStoreRequest = objectStore.add(newItem[0]);        
+      // Make a request to add our newItem object to the object store
+      var objectStoreRequest = objectStore.add(newItem[0]);
         objectStoreRequest.onsuccess = function(event) {
-          
-          // report the success of our new item going into the database
-          note.innerHTML += '<li>New item added to database.</li>';
-          
+
+          // report the success of our request
+          // (to detect whether it has been succesfully
+          // added to the database, you'd look at transaction.oncomplete)
+          note.innerHTML += '<li>Request successful.</li>';
+
           // clear the form, ready for adding the next entry
           title.value = '';
           hours.value = null;
@@ -197,15 +199,15 @@ window.onload = function() {
           day.value = 01;
           month.value = 'January';
           year.value = 2020;
-          
+
         };
-         
+
       };
-      
+
     };
-  
+
   function deleteItem(event) {
-    // retrieve the name of the task we want to delete 
+    // retrieve the name of the task we want to delete
     var dataTask = event.target.getAttribute('data-task');
 
     // open a database transaction and delete the task, finding it by the name we retrieved above
@@ -219,14 +221,14 @@ window.onload = function() {
       note.innerHTML += '<li>Task \"' + dataTask + '\" deleted.</li>';
     };
   };
-  
+
   // this function checks whether the deadline for each task is up or not, and responds appropriately
   function checkDeadlines() {
-    
-    // grab the time and date right now 
+
+    // grab the time and date right now
     var now = new Date();
-    
-    // from the now variable, store the current minutes, hours, day of the month (getDate is needed for this, as getDay 
+
+    // from the now variable, store the current minutes, hours, day of the month (getDate is needed for this, as getDay
     // returns the day of the week, 1-7), month, year (getFullYear needed; getYear is deprecated, and returns a weird value
     // that is not much use to anyone!) and seconds
     var minuteCheck = now.getMinutes();
@@ -234,14 +236,14 @@ window.onload = function() {
     var dayCheck = now.getDate();
     var monthCheck = now.getMonth();
     var yearCheck = now.getFullYear();
-     
-    // again, open a transaction then a cursor to iterate through all the data items in the IDB   
+
+    // again, open a transaction then a cursor to iterate through all the data items in the IDB
     var objectStore = db.transaction(['toDoList'], "readwrite").objectStore('toDoList');
     objectStore.openCursor().onsuccess = function(event) {
       var cursor = event.target.result;
         if(cursor) {
-        
-        // convert the month names we have installed in the IDB into a month number that JavaScript will understand. 
+
+        // convert the month names we have installed in the IDB into a month number that JavaScript will understand.
         // The JavaScript date object creates month values as a number between 0 and 11.
         switch(cursor.value.month) {
           case "January":
@@ -289,19 +291,19 @@ window.onload = function() {
           // The secondsCheck = 0 check is so that you don't get duplicate notifications for the same task. The notification
           // will only appear when the seconds is 0, meaning that you won't get more than one notification for each task
           if(+(cursor.value.hours) == hourCheck && +(cursor.value.minutes) == minuteCheck && +(cursor.value.day) == dayCheck && monthNumber == monthCheck && cursor.value.year == yearCheck && cursor.value.notified == "no") {
-            
+
             // If the numbers all do match, run the createNotification() function to create a system notification
             createNotification(cursor.value.taskTitle);
           }
-          
+
           // move on and perform the same deadline check on the next cursor item
           cursor.continue();
         }
-        
+
     }
-    
+
   }
-  
+
   // function for creating the notification
   function createNotification(title) {
 
@@ -313,11 +315,11 @@ window.onload = function() {
     // Let's check if the user is okay to get some notification
     else if (Notification.permission === "granted") {
       // If it's okay let's create a notification
-      
+
       var img = '/to-do-notifications/img/icon-128.png';
       var text = 'HEY! Your task "' + title + '" is now overdue.';
       var notification = new Notification('To do list', { body: text, icon: img });
-      
+
       window.navigator.vibrate(500);
     }
 
@@ -337,13 +339,13 @@ window.onload = function() {
           var img = '/to-do-notifications/img/icon-128.png';
           var text = 'HEY! Your task "' + title + '" is now overdue.';
           var notification = new Notification('To do list', { body: text, icon: img });
-          
+
           window.navigator.vibrate(500);
         }
       });
     }
 
-    // At last, if the user already denied any notification, and you 
+    // At last, if the user already denied any notification, and you
     // want to be respectful there is no need to bother him any more.
 
     // now we need to update the value of notified to "yes" in this particular data object, so the
@@ -358,20 +360,20 @@ window.onload = function() {
     objectStoreTitleRequest.onsuccess = function() {
       // grab the data object returned as the result
       var data = objectStoreTitleRequest.result;
-      
+
       // update the notified value in the object to "yes"
       data.notified = "yes";
-      
+
       // create another request that inserts the item back into the database
       var updateTitleRequest = objectStore.put(data);
-      
+
       // when this new request succeeds, run the displayData() function again to update the display
       updateTitleRequest.onsuccess = function() {
         displayData();
       }
     }
   }
-  
+
   // using a setInterval to run the checkDeadlines() function every second
   setInterval(checkDeadlines, 1000);
 }
