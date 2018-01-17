f963e67d82440f8631e630043748e1fa83341276
mxschumacher <maximilianbschumacher@gmail.com>
2018-01-17 23:59:01 +0100

    Corrected display of JS-object in HTML by using JSON.stringify

--- web-storage/event.js
+++ web-storage/event.js
@@ -1,7 +1,7 @@
-window.addEventListener('storage', function(e) {  
+window.addEventListener('storage', function(e) {
   document.querySelector('.my-key').textContent = e.key;
   document.querySelector('.my-old').textContent = e.oldValue;
   document.querySelector('.my-new').textContent = e.newValue;
   document.querySelector('.my-url').textContent = e.url;
-  document.querySelector('.my-storage').textContent = e.storageArea;
+  document.querySelector('.my-storage').textContent = JSON.stringify(e.storageArea);
 });
