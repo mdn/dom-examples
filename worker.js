a2156668da626de301c13e9fab2174c73650d7e5
Jhonatan <jhonatangiraldo111@gmail.com>
2019-02-12 18:50:16 -0500

    Validation: It is validated if the inputs are valid numbers

--- worker.js
+++ worker.js
@@ -1,6 +1,18 @@
+function isInvalidNumbers(e) {
+  const { data } = e;
+  return data[0].trim() === ''
+    || isNaN(data[0])
+    || data[1].trim() === ''
+    || isNaN(data[1]);
+}
+
 onmessage = function(e) {
   console.log('Worker: Message received from main script');
-  var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
+  if (isInvalidNumbers(e)) {
+    postMessage('Please write two numbers');
+    return;
+  }
+  var workerResult = 'Result: ' + (parseInt(e.data[0]) * parseInt(e.data[1]));
   console.log('Worker: Posting message back to main script');
   postMessage(workerResult);
 }
