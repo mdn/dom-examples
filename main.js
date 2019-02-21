a37ad347a073299b35f804fe67cc7d92c719c78f
Chris Mills <cmills@mozilla.com>
2019-02-21 09:27:52 +0000

    updating example to provide a working check that the numbers submitted are in fact both numbers

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
-var first = document.querySelector('#number1');
-var second = document.querySelector('#number2');
+const first = document.querySelector('#number1');
+const second = document.querySelector('#number2');
 
-var result = document.querySelector('.result');
+const result = document.querySelector('.result');
 
-if (window.Worker) { // Check if Browser supports the Worker api.
-	// Requires script name as input
-	var myWorker = new Worker("worker.js");
-
-// onkeyup could be used instead of onchange if you wanted to update the answer every time
-// an entered value is changed, and you don't want to have to unfocus the field to update its .value
+if (!!window.Worker) {
+	const myWorker = new Worker("worker.js");
 
 	first.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]); // Sending message as an array to the worker
-	  console.log('Main (first.onchange): Message posted to worker');
+	  myWorker.postMessage([first.value,second.value]);
+	  console.log('Message posted to worker');
 	}
 
 	second.onchange = function() {
 	  myWorker.postMessage([first.value,second.value]);
-	  console.log('Main (second.onchange): Message posted to worker');
+	  console.log('Message posted to worker');
 	}
 
 	myWorker.onmessage = function(e) {
 		result.textContent = e.data;
-		console.log('Main (myWorker.onmessage): Message received from worker');
+		console.log('Message received from worker');
 	}
 }
--- worker.js
+++ worker.js
@@ -1,18 +1,11 @@
-function isInvalidNumbers(e) {
-  const { data } = e;
-  return data[0].trim() === ''
-    || isNaN(data[0])
-    || data[1].trim() === ''
-    || isNaN(data[1]);
-}
-
 onmessage = function(e) {
   console.log('Worker: Message received from main script');
-  if (isInvalidNumbers(e)) {
+  let result = e.data[0] * e.data[1];
+  if (isNaN(result)) {
     postMessage('Please write two numbers');
-    return;
+  } else {
+    let workerResult = 'Result: ' + result;
+    console.log('Worker: Posting message back to main script');
+    postMessage(workerResult);
   }
-  var workerResult = 'Result: ' + (parseInt(e.data[0]) * parseInt(e.data[1]));
-  console.log('Worker: Posting message back to main script');
-  postMessage(workerResult);
 }
