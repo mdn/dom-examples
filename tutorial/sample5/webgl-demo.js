8544e786cd89bdaa239a8267a28198dbdb105dd0
greg nye <gnye450@gmail.com>
2020-05-24 19:37:23 +0100

    Minor correction to code comments and addition of code to rotate cube in the x-axis

--- tutorial/sample5/webgl-demo.js
+++ tutorial/sample5/webgl-demo.js
@@ -254,8 +254,12 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
   mat4.rotate(modelViewMatrix,  // destination matrix
               modelViewMatrix,  // matrix to rotate
               cubeRotation * .7,// amount to rotate in radians
-              [0, 1, 0]);       // axis to rotate around (X)
-
+              [0, 1, 0]);       // axis to rotate around (Y)
+  mat4.rotate(modelViewMatrix,  // destination matrix
+              modelViewMatrix,  // matrix to rotate
+              cubeRotation * .3,// amount to rotate in radians
+              [1, 0, 0]);       // axis to rotate around (X)
+  
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute
   {
