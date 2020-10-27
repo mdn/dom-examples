45a1c9a91c433dd2f65540fdffaf68f1ce16afb3
Germain <germain@grabyo.com>
2020-10-27 12:42:47 +0000

    improve grayscaling example to be able to revert to original

--- canvas/pixel-manipulation/color-manipulation.js
+++ canvas/pixel-manipulation/color-manipulation.js
@@ -1,39 +1,46 @@
 var img = new Image();
 img.crossOrigin = 'anonymous';
 img.src = './assets/rhino.jpg';
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
 img.onload = function() {
-  draw(this);
+	ctx.drawImage(img, 0, 0);
 };
 
-function draw(img) {
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
-  img.style.display = 'none';
-  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  var data = imageData.data;
+var original = function() {
+	ctx.drawImage(img, 0, 0);
+};
 
-  var invert = function() {
-    for (var i = 0; i < data.length; i += 4) {
-      data[i]     = 255 - data[i];     // red
-      data[i + 1] = 255 - data[i + 1]; // green
-      data[i + 2] = 255 - data[i + 2]; // blue
-    }
-    ctx.putImageData(imageData, 0, 0);
-  };
+var invert = function() {
+	ctx.drawImage(img, 0, 0);
+	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	const data = imageData.data;
+	for (var i = 0; i < data.length; i += 4) {
+		data[i]     = 255 - data[i];     // red
+		data[i + 1] = 255 - data[i + 1]; // green
+		data[i + 2] = 255 - data[i + 2]; // blue
+	}
+	ctx.putImageData(imageData, 0, 0);
+};
 
-  var grayscale = function() {
-    for (var i = 0; i < data.length; i += 4) {
-      var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-      data[i]     = avg; // red
-      data[i + 1] = avg; // green
-      data[i + 2] = avg; // blue
-    }
-    ctx.putImageData(imageData, 0, 0);
-  };
+var grayscale = function() {
+	ctx.drawImage(img, 0, 0);
+	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	const data = imageData.data;
+	for (var i = 0; i < data.length; i += 4) {
+		var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+		data[i]     = avg; // red
+		data[i + 1] = avg; // green
+		data[i + 2] = avg; // blue
+	}
+	ctx.putImageData(imageData, 0, 0);
+};
 
-  var invertbtn = document.getElementById('invertbtn');
-  invertbtn.addEventListener('click', invert);
-  var grayscalebtn = document.getElementById('grayscalebtn');
-  grayscalebtn.addEventListener('click', grayscale);
-}
+var originalbtn = document.getElementById('originalbtn');
+originalbtn.addEventListener('click', original);
+var invertbtn = document.getElementById('invertbtn');
+invertbtn.addEventListener('click', invert);
+var grayscalebtn = document.getElementById('grayscalebtn');
+grayscalebtn.addEventListener('click', grayscale);
