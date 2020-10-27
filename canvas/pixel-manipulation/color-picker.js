fa3a56284f1cb0b51ba2c60ec6383b29eeee1917
Germain <germain@grabyo.com>
2020-10-27 13:47:59 +0000

    improve color picker to add selectedColor section

--- canvas/pixel-manipulation/color-picker.js
+++ canvas/pixel-manipulation/color-picker.js
@@ -7,15 +7,26 @@ img.onload = function() {
   ctx.drawImage(img, 0, 0);
   img.style.display = 'none';
 };
-var color = document.getElementById('color');
-function pick(event) {
+var hoveredColor = document.getElementById('hovered-color');
+var selectedColor = document.getElementById('selected-color');
+
+
+function pick(event, destination) {
   var x = event.layerX;
   var y = event.layerY;
   var pixel = ctx.getImageData(x, y, 1, 1);
   var data = pixel.data;
-  var rgba = 'rgba(' + data[0] + ', ' + data[1] +
-             ', ' + data[2] + ', ' + (data[3] / 255) + ')';
-  color.style.background =  rgba;
-  color.textContent = rgba;
+
+	const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+	destination.style.background = rgba;
+	destination.textContent = rgba;
+
+	return rgba;
 }
-canvas.addEventListener('mousemove', pick);
+
+canvas.addEventListener('mousemove', function(event) {
+	pick(event, hoveredColor);
+});
+canvas.addEventListener('click', function(event) {
+	pick(event, selectedColor);
+});
