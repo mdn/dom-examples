2f6b5013622760aea2e3e2e153a01ab7c13b6a34
Germain <germain@grabyo.com>
2020-10-27 12:59:46 +0000

    improve image smoothing example

--- canvas/pixel-manipulation/image-smoothing.js
+++ canvas/pixel-manipulation/image-smoothing.js
@@ -9,28 +9,29 @@ function draw(img) {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   ctx.drawImage(img, 0, 0);
-  img.style.display = 'none';
-  var zoomctx = document.getElementById('zoom').getContext('2d');
 
-  var smoothbtn = document.getElementById('smoothbtn');
-  var toggleSmoothing = function(event) {
-    zoomctx.imageSmoothingEnabled = this.checked;
-    zoomctx.mozImageSmoothingEnabled = this.checked;
-    zoomctx.webkitImageSmoothingEnabled = this.checked;
-    zoomctx.msImageSmoothingEnabled = this.checked;
-  };
-  smoothbtn.addEventListener('change', toggleSmoothing);
+	var smoothedZoomCtx = document.getElementById('smoothed-zoom').getContext('2d');
+	smoothedZoomCtx.imageSmoothingEnabled = this.checked;
+	smoothedZoomCtx.mozImageSmoothingEnabled = this.checked;
+	smoothedZoomCtx.webkitImageSmoothingEnabled = this.checked;
+	smoothedZoomCtx.msImageSmoothingEnabled = this.checked;
 
-  var zoom = function(event) {
-    var x = event.layerX;
-    var y = event.layerY;
-    zoomctx.drawImage(canvas,
-                      Math.min(Math.max(0, x - 5), img.width - 10),
-                      Math.min(Math.max(0, y - 5), img.height - 10),
-                      10, 10,
-                      0, 0,
-                      200, 200);
+	var pixelatedZoomCtx = document.getElementById('pixelated-zoom').getContext('2d');
+
+
+  var zoom = function(ctx, x, y) {
+    ctx.drawImage(canvas,
+									Math.min(Math.max(0, x - 5), img.width - 10),
+									Math.min(Math.max(0, y - 5), img.height - 10),
+									10, 10,
+									0, 0,
+									200, 200);
   };
 
-  canvas.addEventListener('mousemove', zoom);
+  canvas.addEventListener('mousemove', function(event) {
+		const x = event.layerX;
+		const y = event.layerY;
+    zoom(smoothedZoomCtx, x, y);
+    zoom(pixelatedZoomCtx, x, y);
+	});
 }
