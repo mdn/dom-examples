76c19569f5412ed3e52b28c3b1e6099f67dac934
Valentin <valentyn.patsera@gmail.com>
2022-03-20 11:37:27 +0100

    replace string concatanation with backtick

--- scripts/todo.js
+++ scripts/todo.js
@@ -340,7 +340,7 @@ window.onload = function() {
 
     // Create and show the notification
     const img = '/to-do-notifications/img/icon-128.png';
-    const text = 'HEY! Your task "' + title + '" is now overdue.';
+    const text = `HEY! Your task "${title}" is now overdue.`;
     const notification = new Notification('To do list', { body: text, icon: img });
 
     // we need to update the value of notified to 'yes' in this particular data object, so the
