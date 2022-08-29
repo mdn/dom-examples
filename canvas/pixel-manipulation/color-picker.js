94b5a8122dc2ed8594a600a3c73d7efc9fc67128
Will Bamberg <will@bootbonnet.ca>
2022-08-29 15:22:27 -0700

    var to const, and use arrow functions

--- canvas/pixel-manipulation/color-picker.js
+++ canvas/pixel-manipulation/color-picker.js
@@ -1,14 +1,14 @@
-var img = new Image();
+const img = new Image();
 img.crossOrigin = 'anonymous';
-img.src = './assets/rhino.jpg';
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-img.onload = function () {
+img.src = '/assets/rhino.jpg';
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+img.addEventListener('load', () => {
   ctx.drawImage(img, 0, 0);
   img.style.display = 'none';
-};
-var hoveredColor = document.getElementById('hovered-color');
-var selectedColor = document.getElementById('selected-color');
+});
+const hoveredColor = document.getElementById('hovered-color');
+const selectedColor = document.getElementById('selected-color');
 
 
 function pick(event, destination) {
@@ -25,9 +25,5 @@ function pick(event, destination) {
   return rgba;
 }
 
-canvas.addEventListener('mousemove', function (event) {
-  pick(event, hoveredColor);
-});
-canvas.addEventListener('click', function (event) {
-  pick(event, selectedColor);
-});
+canvas.addEventListener('mousemove', event => pick(event, hoveredColor));
+canvas.addEventListener('click', event => pick(event, selectedColor));
