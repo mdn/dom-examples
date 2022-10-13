aa1c468e4c9b0e7f85cedf9c9cbc56b1f0475a98
Hubert Boma Manilla <b4bomsy@gmail.com>
2022-10-13 02:07:42 +0200

    Fix the sw.js patch

--- service-worker/simple-service-worker/app.js
+++ service-worker/simple-service-worker/app.js
@@ -4,7 +4,7 @@ const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
       const registration = await navigator.serviceWorker.register(
-        '/sw.js',
+        'sw.js',
         {
           scope: '/',
         }
