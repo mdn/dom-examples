5b29191f12a5b11760e5473e7abb65d2c12a8573
Chris Mills <chrisdavidmills@gmail.com>
2023-02-08 08:22:51 +0000

    Indentation and small code fixes

--- webgpu-compute-demo/script.js
+++ webgpu-compute-demo/script.js
@@ -1,19 +1,24 @@
-// Compute shader
+// Define global buffer size
+const BUFFER_SIZE = 1000; 
 
+// Compute shader
 const shader = `
 @group(0) @binding(0)
 var<storage, read_write> output: array<f32>;
 
 @compute @workgroup_size(64)
 fn main(
-
   @builtin(global_invocation_id)
   global_id : vec3u,
 
   @builtin(local_invocation_id)
   local_id : vec3u,
-
 ) {
+  // Avoid accessing the buffer out of bounds
+  if (global_id.x >= ${BUFFER_SIZE}) {
+    return;
+  }
+
   output[global_id.x] =
     f32(global_id.x) * 1000. + f32(local_id.x);
 }
@@ -22,113 +27,107 @@ fn main(
 // Main function
 
 async function init() {
-    // 1: request adapter and device
-    if (!navigator.gpu) {
-        throw Error('WebGPU not supported.');
-    }
+  // 1: request adapter and device
+  if (!navigator.gpu) {
+    throw Error('WebGPU not supported.');
+  }
 
-    const adapter = await navigator.gpu.requestAdapter();
-    if (!adapter) {
-        throw Error('Couldn\'t request WebGPU adapter.');
-    }
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    throw Error('Couldn\'t request WebGPU adapter.');
+  }
 
-    const device = await adapter.requestDevice();
-    if (!device) {
-        throw Error('Couldn\'t request WebGPU logical device.');
-    }
+  const device = await adapter.requestDevice();
 
-    // 2: Create a shader module from the shader template literal
-    const shaderModule = device.createShaderModule({
-        code: shader
-    });
+  // 2: Create a shader module from the shader template literal
+  const shaderModule = device.createShaderModule({
+    code: shader
+  });
 
-    // 3: Create an output buffer to read GPU calculations to, a staging buffer to be mapped for JavaScript access,
-    // and a GPUBindGroup based on the GPUBindGroupLayout
+  // 3: Create an output buffer to read GPU calculations to, a staging buffer to be mapped for JavaScript access,
+  // and a GPUBindGroup based on the GPUBindGroupLayout
 
-    const BUFFER_SIZE = 1000;
+  const output = device.createBuffer({
+    size: BUFFER_SIZE,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+  });
 
-    const output = device.createBuffer({
-        size: BUFFER_SIZE,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
-    });
+  const stagingBuffer = device.createBuffer({
+    size: BUFFER_SIZE,
+    usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+  });
 
-    const stagingBuffer = device.createBuffer({
-        size: BUFFER_SIZE,
-        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
-    });
+  // 4: Create a GPUBindGroupLayout to define the bind group structure, create a GPUBindGroup from it,
+  // then use it to create a GPUComputePipeline
 
-    // 4: Create a GPUBindGroupLayout to define the bind group structure, create a GPUBindGroup from it,
-    // then use it to create a GPUComputePipeline
+  const bindGroupLayout =
+  device.createBindGroupLayout({
+    entries: [{
+      binding: 0,
+      visibility: GPUShaderStage.COMPUTE,
+      buffer: {
+        type: "storage"
+      }
+    }]
+  });
 
-    const bindGroupLayout =
-    device.createBindGroupLayout({
-        entries: [{
-            binding: 0,
-            visibility: GPUShaderStage.COMPUTE,
-            buffer: {
-                type: "storage"
-            }
-        }]
-    });
+  const bindGroup = device.createBindGroup({
+    layout: bindGroupLayout,
+    entries: [{
+      binding: 0,
+      resource: {
+        buffer: output,
+      }
+    }]
+  });
 
-    const bindGroup = device.createBindGroup({
-        layout: bindGroupLayout,
-        entries: [{
-            binding: 0,
-            resource: {
-              buffer: output,
-            }
-        }]
-    });
+  const computePipeline = device.createComputePipeline({
+    layout: device.createPipelineLayout({
+      bindGroupLayouts: [bindGroupLayout]
+    }),
+    compute: {
+      module: shaderModule,
+      entryPoint: 'main'
+    },
+  });
 
-    const computePipeline = device.createComputePipeline({
-        layout: device.createPipelineLayout({
-            bindGroupLayouts: [bindGroupLayout]
-        }),
-        compute: {
-          module: shaderModule,
-          entryPoint: 'main'
-        },
-    });
+  // 5: Create GPUCommandEncoder to issue commands to the GPU
+  const commandEncoder = device.createCommandEncoder();
 
-    // 5: Create GPUCommandEncoder to issue commands to the GPU
-    const commandEncoder = device.createCommandEncoder();
-
-    // 6: Initiate render pass
-    const passEncoder = commandEncoder.beginComputePass();
+  // 6: Initiate render pass
+  const passEncoder = commandEncoder.beginComputePass();
     
-    // 7: Issue commands
+  // 7: Issue commands
+  passEncoder.setPipeline(computePipeline);
+  passEncoder.setBindGroup(0, bindGroup);
+  passEncoder.dispatchWorkgroups(Math.ceil(BUFFER_SIZE / 64));
 
-    passEncoder.setPipeline(computePipeline);
-    passEncoder.setBindGroup(0, bindGroup);
-    passEncoder.dispatchWorkgroups(Math.ceil(BUFFER_SIZE / 64));
+  // End the render pass
+  passEncoder.end();
 
-    // End the render pass
-    passEncoder.end();
+  // Copy output buffer to staging buffer
+  commandEncoder.copyBufferToBuffer(
+    output,
+    0, // Source offset
+    stagingBuffer,
+    0, // Destination offset
+    BUFFER_SIZE
+  );
 
-    // Copy output buffer to staging buffer
-    commandEncoder.copyBufferToBuffer(
-        output,
-        0, // Source offset
-        stagingBuffer,
-        0, // Destination offset
-        BUFFER_SIZE
-    );
+  // 8: End frame by passing array of command buffers to command queue for execution
+  device.queue.submit([commandEncoder.finish()]);
 
-    // 8: End frame by passing array of command buffers to command queue for execution
-    device.queue.submit([commandEncoder.finish()]);
+  // map staging buffer to read results back to JS
+  await stagingBuffer.mapAsync(
+    GPUMapMode.READ,
+    0, // Offset
+    BUFFER_SIZE // Length
+  );
 
-    // map staging buffer to read results back to JS
-    await stagingBuffer.mapAsync(
-        GPUMapMode.READ,
-        0, // Offset
-        BUFFER_SIZE // Length
-    );
-
-    const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
-    const data = copyArrayBuffer.slice();
-    stagingBuffer.unmap();
-    console.log(new Float32Array(data));
+  const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
+  const data = copyArrayBuffer.slice();
+  stagingBuffer.unmap();
+  console.log(new Float32Array(data));
 }
 
 init();
--- webgpu-render-demo/script.js
+++ webgpu-render-demo/script.js
@@ -5,22 +5,22 @@ const clearColor = { r: 0.0, g: 0.5, b: 1.0, a: 1.0 };
 // Each vertex has 8 values representing position and color: X Y Z W R G B A
 
 const vertices = new Float32Array([
-    0.0,  0.6, 0, 1, 1, 0, 0, 1,
-   -0.5, -0.6, 0, 1, 0, 1, 0, 1,
-    0.5, -0.6, 0, 1, 0, 0, 1, 1
+  0.0,  0.6, 0, 1, 1, 0, 0, 1,
+ -0.5, -0.6, 0, 1, 0, 1, 0, 1,
+  0.5, -0.6, 0, 1, 0, 0, 1, 1
 ]);
 
 // Vertex and fragment shaders
 
 const shaders = `
 struct VertexOut {
-    @builtin(position) position : vec4f,
-    @location(0) color : vec4f
+  @builtin(position) position : vec4f,
+  @location(0) color : vec4f
 }
 
 @vertex
 fn vertex_main(@location(0) position: vec4f,
-                 @location(1) color: vec4f) -> VertexOut
+               @location(1) color: vec4f) -> VertexOut
 {
   var output : VertexOut;
   output.position = position;
@@ -38,117 +38,111 @@ fn fragment_main(fragData: VertexOut) -> @location(0) vec4f
 // Main function
 
 async function init() {
-    // 1: request adapter and device
-    if (!navigator.gpu) {
-        throw Error('WebGPU not supported.');
-    }
+  // 1: request adapter and device
+  if (!navigator.gpu) {
+    throw Error('WebGPU not supported.');
+  }
 
-    const adapter = await navigator.gpu.requestAdapter();
-    if (!adapter) {
-        throw Error('Couldn\'t request WebGPU adapter.');
-    }
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    throw Error('Couldn\'t request WebGPU adapter.');
+  }
     
-    const adapterInfo = await adapter.requestAdapterInfo([]);
-    console.log(adapterInfo);
+  // const adapterInfo = await adapter.requestAdapterInfo([]);
+  // console.log(adapterInfo);
 
-    const device = await adapter.requestDevice();
-    if (!device) {
-        throw Error('Couldn\'t request WebGPU logical device.');
-    }
+  const device = await adapter.requestDevice();
 
-    // 2: Create a shader module from the shaders template literal
-    const shaderModule = device.createShaderModule({
-        code: shaders
-    });
+  // 2: Create a shader module from the shaders template literal
+  const shaderModule = device.createShaderModule({
+    code: shaders
+  });
 
-    // 3: Get reference to the canvas to render on
-    const canvas = document.querySelector('#gpuCanvas');
-    const context = canvas.getContext('webgpu');
+  // 3: Get reference to the canvas to render on
+  const canvas = document.querySelector('#gpuCanvas');
+  const context = canvas.getContext('webgpu');
 
-    context.configure({
-        device: device,
-        format: navigator.gpu.getPreferredCanvasFormat(),
-        alphaMode: 'premultiplied'
-    });
+  context.configure({
+    device: device,
+    format: navigator.gpu.getPreferredCanvasFormat(),
+    alphaMode: 'premultiplied'
+  });
 
-    // 4: Create vertex buffer to contain vertex data
-    const vertexBuffer = device.createBuffer({
-        size: vertices.byteLength, // make it big enough to store vertices in
-        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-    });
+  // 4: Create vertex buffer to contain vertex data
+  const vertexBuffer = device.createBuffer({
+    size: vertices.byteLength, // make it big enough to store vertices in
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
 
-    // Store vertices in a typed array, then copy the vertex data over to the GPUBuffer using writeBuffer() utility function
-    const array = new Float32Array(vertices);
-    device.queue.writeBuffer(vertexBuffer, 0, array, 0, array.length);
+  // Copy the vertex data over to the GPUBuffer using the writeBuffer() utility function
+  device.queue.writeBuffer(vertexBuffer, 0, vertices, 0, vertices.length);
 
-    // 5: Create a GPUVertexBufferLayout and GPURenderPipelineDescriptor to provide a definition of our render pipline
-    const vertexBuffers = [{
-        attributes: [{
-            shaderLocation: 0, // position
-            offset: 0,
-            format: 'float32x4'
-        }, {
-            shaderLocation: 1, // color
-            offset: 16,
-            format: 'float32x4'
-        }],
-        arrayStride: 32,
-        stepMode: 'vertex'
-    }];
+  // 5: Create a GPUVertexBufferLayout and GPURenderPipelineDescriptor to provide a definition of our render pipline
+  const vertexBuffers = [{
+    attributes: [{
+      shaderLocation: 0, // position
+      offset: 0,
+      format: 'float32x4'
+    }, {
+      shaderLocation: 1, // color
+      offset: 16,
+      format: 'float32x4'
+    }],
+    arrayStride: 32,
+    stepMode: 'vertex'
+  }];
 
-    const pipelineDescriptor = {
-        vertex: {
-            module: shaderModule,
-            entryPoint: 'vertex_main',
-            buffers: vertexBuffers
-        },
-        fragment: {
-            module: shaderModule,
-            entryPoint: 'fragment_main',
-            targets: [{
-              format: navigator.gpu.getPreferredCanvasFormat()
-            }]
-        },
-        primitive: {
-            topology: 'triangle-list'
-        },
-        layout: 'auto'
-    };
+  const pipelineDescriptor = {
+    vertex: {
+      module: shaderModule,
+      entryPoint: 'vertex_main',
+      buffers: vertexBuffers
+    },
+    fragment: {
+      module: shaderModule,
+      entryPoint: 'fragment_main',
+      targets: [{
+        format: navigator.gpu.getPreferredCanvasFormat()
+      }]
+    },
+    primitive: {
+      topology: 'triangle-list'
+    },
+    layout: 'auto'
+  };
 
-    // 6: Create the actual render pipline
+  // 6: Create the actual render pipline
 
-    const renderPipeline = device.createRenderPipeline(pipelineDescriptor);
+  const renderPipeline = device.createRenderPipeline(pipelineDescriptor);
     
+  // 7: Create GPUCommandEncoder to issue commands to the GPU
+  // Note: render pass descriptor, command encoder, etc. are destroyed after use, fresh one needed for each frame.
+  const commandEncoder = device.createCommandEncoder();
+
+  // 8: Create GPURenderPassDescriptor to tell WebGPU which texture to draw into, then initiate render pass
+
+  const renderPassDescriptor = {
+    colorAttachments: [{
+      clearValue: clearColor,
+      loadOp: 'clear',
+      storeOp: 'store',
+      view: context.getCurrentTexture().createView()
+    }]
+  };
+
+  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
     
+  // 9: Draw the triangle
 
-    // 7: Create GPUCommandEncoder to issue commands to the GPU
-    // Note: render pass descriptor, command encoder, etc. are destroyed after use, fresh one needed for each frame.
-    const commandEncoder = device.createCommandEncoder();
+  passEncoder.setPipeline(renderPipeline);
+  passEncoder.setVertexBuffer(0, vertexBuffer);
+  passEncoder.draw(3);
 
-    // 8: Create GPURenderPassDescriptor to tell WebGPU which texture to draw into, then initiate render pass
+  // End the render pass
+  passEncoder.end();
 
-    const renderPassDescriptor = {
-        colorAttachments: [{
-            clearValue: clearColor,
-            loadOp: 'clear',
-            storeOp: 'store',
-            view: context.getCurrentTexture().createView()
-        }]
-    };
-
-    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-    
-    // 9: Draw the triangle
-
-    passEncoder.setPipeline(renderPipeline);
-    passEncoder.setVertexBuffer(0, vertexBuffer);
-    passEncoder.draw(3);
-
-    // End the render pass
-    passEncoder.end();
-
-    // 10: End frame by passing array of command buffers to command queue for execution
-    device.queue.submit([commandEncoder.finish()]);
+  // 10: End frame by passing array of command buffers to command queue for execution
+  device.queue.submit([commandEncoder.finish()]);
 }
 
 init();
