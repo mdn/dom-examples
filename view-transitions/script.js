2facb7d9df50982cd674b40dba3b9759d7a328b4
Chris Mills <chrisdavidmills@gmail.com>
2023-02-22 16:32:26 +0000

    demo updates

--- view-transitions/script.js
+++ view-transitions/script.js
@@ -24,18 +24,37 @@ const galleryCaption = document.querySelector('.gallery-view figcaption');
 function init() {
   imageData.forEach(data => {
     const img = document.createElement('img');
+    const a = document.createElement('a');
+    a.href = "#";
+    a.title = `Click to load ${data.name} in main gallery view`;
     img.alt = data.name;
     img.src = `images/${ data.file }_th.jpg`;
-    thumbs.appendChild(img);
+    a.appendChild(img);
+    thumbs.appendChild(a);
     
-    img.addEventListener('click', updateView);
+    a.addEventListener('click', updateView);
+    a.addEventListener('keypress', updateView);
   }) 
   
   galleryImg.src = `images/${ imageData[0].file }.jpg`;
   galleryCaption.textContent = imageData[0].name;
 }
 
-function updateView(e) {
+function updateView(event) {
+  // Handle the difference in whether the event is fired on the <a> or the <img>
+  let targetIdentifier;
+  if (event.target.firstChild === null) {
+    targetIdentifier = event.target;
+  } else {
+    targetIdentifier = event.target.firstChild;
+  }
+
+  const displayNewImage = () => {
+    const mainSrc = `${targetIdentifier.src.split("_th.jpg")[0]}.jpg`;
+    galleryImg.src = mainSrc;
+    galleryCaption.textContent = targetIdentifier.alt;
+  };
+  
   // Fallback for browsers that don't support View Transitions:
   if (!document.startViewTransition) {
     displayNewImage();
@@ -43,13 +62,7 @@ function updateView(e) {
   }
 
   // With View Transitions:
-  document.startViewTransition(() => displayNewImage());
-  
-  function displayNewImage() {
-    const mainSrc = `${e.target.src.split('_th.jpg')[0]}.jpg`;
-    galleryImg.src = mainSrc;
-    galleryCaption.textContent = e.target.alt;
-  }
+  const transition = document.startViewTransition(() => displayNewImage());
 }
 
 init();
