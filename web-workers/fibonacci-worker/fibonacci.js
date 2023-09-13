969fad2352e6e87e54ff4f038973947503e02021
wbamberg <will@bootbonnet.ca>
2023-09-12 17:12:36 -0700

    Fix issue 28972

--- web-workers/fibonacci-worker/fibonacci.js
+++ web-workers/fibonacci-worker/fibonacci.js
@@ -1,19 +1,8 @@
 self.onmessage = function (event) {
   const userNum = Number(event.data);
-  fibonacci(userNum);
+  self.postMessage(fibonacci(userNum));
 };
 
-function fibonacci(num) {
-  let a = 1;
-  let b = 0;
-  let temp;
-
-  while (num >= 0) {
-    temp = a;
-    a = a + b;
-    b = temp;
-    num--;
-  }
-
-  self.postMessage(b);
+function fibonacci(n) {
+  return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
