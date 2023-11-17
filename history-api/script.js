3af31d5e604f5204a9735301121661d6fffa1d6b
wbamberg <will@bootbonnet.ca>
2023-11-16 17:20:20 -0800

    Set alt

--- history-api/script.js
+++ history-api/script.js
@@ -1,39 +1,45 @@
 function displayContent(state) {
-	const description = document.querySelector("#description");
-	description.textContent = state.description;
+  const description = document.querySelector("#description");
+  description.textContent = state.description;
 
-	const photo = document.querySelector("#photo");
-	photo.setAttribute("src", state.photo);
+  const photo = document.querySelector("#photo");
+  photo.setAttribute("src", state.image.src);
+  photo.setAttribute("alt", state.image.alt);
 }
 
 // handle click on link
 document.addEventListener("click", async (event) => {
-	const cat = event.target.getAttribute("data-cat");
-	if (cat) {
-		event.preventDefault();
-		try {
-			const response = await fetch(`${cat}.json`);
-			const json = await response.json();
-			displayContent(json);
-			history.pushState(json, "", "");
-		} catch (err) {
-			console.error(err);
-		}
-	}
+  const cat = event.target.getAttribute("data-cat");
+  if (cat) {
+    event.preventDefault();
+    try {
+      const response = await fetch(`${cat}.json`);
+      const json = await response.json();
+      displayContent(json);
+      history.pushState(json, "", "");
+    } catch (err) {
+      console.error(err);
+    }
+  }
 });
 
 // handle forward/back buttons
 window.addEventListener("popstate", (event) => {
-	// guard against popstate event on chrome init
-	if (event.state) {
-		// get the state and change the page content
-		displayContent(event.state, true);
-	}
+  // guard against popstate event on chrome init
+  if (event.state) {
+    // get the state and change the page content
+    displayContent(event.state, true);
+  }
 });
+
+const image = document.querySelector("#photo");
 
 // create state on page init and replace the current history with it
 const initialState = {
-	description: document.querySelector("#description").textContent,
-	photo: document.querySelector("#photo").getAttribute("src"),
+  description: document.querySelector("#description").textContent,
+  image: {
+    src: image.getAttribute("src"),
+    alt: image.getAttribute("alt"),
+  },
 };
 history.replaceState(initialState, "", document.location.href);
