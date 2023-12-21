3d1b09fb49115c92a92c8383070baf8e55d7402d
Jean-Yves Perrier <jypenator@gmail.com>
2023-12-21 06:21:12 +0100

    Run Prettier

--- media/web-dictaphone/scripts/app.js
+++ media/web-dictaphone/scripts/app.js
@@ -1,9 +1,9 @@
 // Set up basic variables for app
-const record = document.querySelector('.record');
-const stop = document.querySelector('.stop');
-const soundClips = document.querySelector('.sound-clips');
-const canvas = document.querySelector('.visualizer');
-const mainSection = document.querySelector('.main-controls');
+const record = document.querySelector(".record");
+const stop = document.querySelector(".stop");
+const soundClips = document.querySelector(".sound-clips");
+const canvas = document.querySelector(".visualizer");
+const mainSection = document.querySelector(".main-controls");
 
 // Disable stop button while not recording
 stop.disabled = true;
@@ -14,17 +14,17 @@ const canvasCtx = canvas.getContext("2d");
 
 // Main block for doing the audio recording
 if (navigator.mediaDevices.getUserMedia) {
-  console.log('The mediaDevices.getUserMedia() method is supported.');
+  console.log("The mediaDevices.getUserMedia() method is supported.");
 
   const constraints = { audio: true };
   let chunks = [];
 
-  let onSuccess = function(stream) {
+  let onSuccess = function (stream) {
     const mediaRecorder = new MediaRecorder(stream);
 
     visualize(stream);
 
-    record.onclick = function() {
+    record.onclick = function () {
       mediaRecorder.start();
       console.log(mediaRecorder.state);
       console.log("Recorder started.");
@@ -32,9 +32,9 @@ if (navigator.mediaDevices.getUserMedia) {
 
       stop.disabled = false;
       record.disabled = true;
-    }
+    };
 
-    stop.onclick = function() {
+    stop.onclick = function () {
       mediaRecorder.stop();
       console.log(mediaRecorder.state);
       console.log("Recorder stopped.");
@@ -43,25 +43,28 @@ if (navigator.mediaDevices.getUserMedia) {
 
       stop.disabled = true;
       record.disabled = false;
-    }
+    };
 
-    mediaRecorder.onstop = function(e) {
+    mediaRecorder.onstop = function (e) {
       console.log("Last data to read (after MediaRecorder.stop() called).");
 
-      const clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
+      const clipName = prompt(
+        "Enter a name for your sound clip?",
+        "My unnamed clip"
+      );
 
-      const clipContainer = document.createElement('article');
-      const clipLabel = document.createElement('p');
-      const audio = document.createElement('audio');
-      const deleteButton = document.createElement('button');
+      const clipContainer = document.createElement("article");
+      const clipLabel = document.createElement("p");
+      const audio = document.createElement("audio");
+      const deleteButton = document.createElement("button");
 
-      clipContainer.classList.add('clip');
-      audio.setAttribute('controls', '');
-      deleteButton.textContent = 'Delete';
-      deleteButton.className = 'delete';
+      clipContainer.classList.add("clip");
+      audio.setAttribute("controls", "");
+      deleteButton.textContent = "Delete";
+      deleteButton.className = "delete";
 
-      if(clipName === null) {
-        clipLabel.textContent = 'My unnamed clip';
+      if (clipName === null) {
+        clipLabel.textContent = "My unnamed clip";
       } else {
         clipLabel.textContent = clipName;
       }
@@ -72,44 +75,43 @@ if (navigator.mediaDevices.getUserMedia) {
       soundClips.appendChild(clipContainer);
 
       audio.controls = true;
-      const blob = new Blob(chunks, { 'type' : mediaRecorder.mimeType });
+      const blob = new Blob(chunks, { type: mediaRecorder.mimeType });
       chunks = [];
       const audioURL = window.URL.createObjectURL(blob);
       audio.src = audioURL;
       console.log("recorder stopped");
 
-      deleteButton.onclick = function(e) {
+      deleteButton.onclick = function (e) {
         e.target.closest(".clip").remove();
-      }
+      };
 
-      clipLabel.onclick = function() {
+      clipLabel.onclick = function () {
         const existingName = clipLabel.textContent;
-        const newClipName = prompt('Enter a new name for your sound clip?');
-        if(newClipName === null) {
+        const newClipName = prompt("Enter a new name for your sound clip?");
+        if (newClipName === null) {
           clipLabel.textContent = existingName;
         } else {
           clipLabel.textContent = newClipName;
         }
-      }
-    }
+      };
+    };
 
-    mediaRecorder.ondataavailable = function(e) {
+    mediaRecorder.ondataavailable = function (e) {
       chunks.push(e.data);
-    }
-  }
+    };
+  };
 
-  let onError = function(err) {
-    console.log('The following error occured: ' + err);
-  }
+  let onError = function (err) {
+    console.log("The following error occured: " + err);
+  };
 
   navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
-
 } else {
-   console.log('MediaDevices.getUserMedia() not supported on your browser!');
+  console.log("MediaDevices.getUserMedia() not supported on your browser!");
 }
 
 function visualize(stream) {
-  if(!audioCtx) {
+  if (!audioCtx) {
     audioCtx = new AudioContext();
   }
 
@@ -122,34 +124,32 @@ function visualize(stream) {
 
   source.connect(analyser);
 
-  draw()
+  draw();
 
   function draw() {
-    const WIDTH = canvas.width
+    const WIDTH = canvas.width;
     const HEIGHT = canvas.height;
 
     requestAnimationFrame(draw);
 
     analyser.getByteTimeDomainData(dataArray);
 
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillStyle = "rgb(200, 200, 200)";
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
     canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+    canvasCtx.strokeStyle = "rgb(0, 0, 0)";
 
     canvasCtx.beginPath();
 
-    let sliceWidth = WIDTH * 1.0 / bufferLength;
+    let sliceWidth = (WIDTH * 1.0) / bufferLength;
     let x = 0;
 
-
-    for(let i = 0; i < bufferLength; i++) {
-
+    for (let i = 0; i < bufferLength; i++) {
       let v = dataArray[i] / 128.0;
-      let y = v * HEIGHT/2;
+      let y = (v * HEIGHT) / 2;
 
-      if(i === 0) {
+      if (i === 0) {
         canvasCtx.moveTo(x, y);
       } else {
         canvasCtx.lineTo(x, y);
@@ -158,14 +158,13 @@ function visualize(stream) {
       x += sliceWidth;
     }
 
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
     canvasCtx.stroke();
-
   }
 }
 
-window.onresize = function() {
+window.onresize = function () {
   canvas.width = mainSection.offsetWidth;
-}
+};
 
 window.onresize();
