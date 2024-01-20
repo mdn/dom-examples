76ef518f58fb4a0c6f781feca215e05f6d5945dc
Jean-Xavier Bardant <jxbardant@hotmail.com>
2024-01-20 21:20:38 +0100

    white space

--- web-workers/fibonacci-worker/fibonacci.js
+++ web-workers/fibonacci-worker/fibonacci.js
@@ -7,9 +7,9 @@ function fibonacci(num) {
   let a = 1;
   let b = 0;
   while (num > 0) {
-	a = a+b;
-    b = a-b; // Current a is previous a+ previous b
-			 // In modern JS: [a,b] = [a+b,a]
+  a = a+b;
+  b = a-b; // Current a is previous a+ previous b
+           // In modern JS: [a,b] = [a+b,a]
     num--;
   }
 
