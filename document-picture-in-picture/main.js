4c363ede5684749de45887f09eabe6b01a21dcbe
Josh Farrant <joshfarrant@github.com>
2024-05-08 09:41:31 +0100

    add early return to match docs

--- document-picture-in-picture/main.js
+++ document-picture-in-picture/main.js
@@ -14,52 +14,53 @@ if ("documentPictureInPicture" in window) {
 }
 
 async function togglePictureInPicture() {
-  // Returns null if no pip window is currently open
-  if (!window.documentPictureInPicture.window) {
-    // Open a Picture-in-Picture window.
-    const pipWindow = await window.documentPictureInPicture.requestWindow({
-      width: videoPlayer.clientWidth,
-      height: videoPlayer.clientHeight + 50,
-    });
-
-    // Add pagehide listener to handle the case of the pip window being closed using the browser X button
-    pipWindow.addEventListener("pagehide", (event) => {
-      inPipMessage.style.display = "none";
-      playerContainer.append(videoPlayer);
-    });
-
-    // Copy style sheets over from the initial document
-    // so that the player looks the same.
-    [...document.styleSheets].forEach((styleSheet) => {
-      try {
-        const cssRules = [...styleSheet.cssRules]
-          .map((rule) => rule.cssText)
-          .join("");
-        const style = document.createElement("style");
-
-        style.textContent = cssRules;
-        pipWindow.document.head.appendChild(style);
-      } catch (e) {
-        const link = document.createElement("link");
-
-        link.rel = "stylesheet";
-        link.type = styleSheet.type;
-        link.media = styleSheet.media;
-        link.href = styleSheet.href;
-        pipWindow.document.head.appendChild(link);
-      }
-    });
-
-    // Move the player to the Picture-in-Picture window.
-    pipWindow.document.body.append(videoPlayer);
-
-    // Display a message to say it has been moved
-    inPipMessage.style.display = "block";
-  } else {
+  // Early return if there's already a Picture-in-Picture window open
+  if (window.documentPictureInPicture.window) {
     inPipMessage.style.display = "none";
     playerContainer.append(videoPlayer);
     window.documentPictureInPicture.window.close();
+    return;
   }
+
+  // Open a Picture-in-Picture window.
+  const pipWindow = await window.documentPictureInPicture.requestWindow({
+    width: videoPlayer.clientWidth,
+    height: videoPlayer.clientHeight + 50,
+  });
+
+  // Add pagehide listener to handle the case of the pip window being closed using the browser X button
+  pipWindow.addEventListener("pagehide", (event) => {
+    inPipMessage.style.display = "none";
+    playerContainer.append(videoPlayer);
+  });
+
+  // Copy style sheets over from the initial document
+  // so that the player looks the same.
+  [...document.styleSheets].forEach((styleSheet) => {
+    try {
+      const cssRules = [...styleSheet.cssRules]
+        .map((rule) => rule.cssText)
+        .join("");
+      const style = document.createElement("style");
+
+      style.textContent = cssRules;
+      pipWindow.document.head.appendChild(style);
+    } catch (e) {
+      const link = document.createElement("link");
+
+      link.rel = "stylesheet";
+      link.type = styleSheet.type;
+      link.media = styleSheet.media;
+      link.href = styleSheet.href;
+      pipWindow.document.head.appendChild(link);
+    }
+  });
+
+  // Move the player to the Picture-in-Picture window.
+  pipWindow.document.body.append(videoPlayer);
+
+  // Display a message to say it has been moved
+  inPipMessage.style.display = "block";
 }
 
 documentPictureInPicture.addEventListener("enter", (event) => {
