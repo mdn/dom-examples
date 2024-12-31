8a61cfeefd85e0a86bf1abcbfc6b26713ba9e299
Brian Smith <brian@smith.berlin>
2024-12-31 13:16:13 +0100

    chore: Format web workers dir

--- web-workers/simple-web-worker/main.js
+++ web-workers/simple-web-worker/main.js
@@ -1,22 +1,22 @@
-const first = document.querySelector('#number1');
-const second = document.querySelector('#number2');
+const first = document.querySelector("#number1");
+const second = document.querySelector("#number2");
 
-const result = document.querySelector('.result');
+const result = document.querySelector(".result");
 
 if (window.Worker) {
   const myWorker = new Worker("worker.js");
 
-  [first, second].forEach(input => {
-    input.onchange = function() {
+  [first, second].forEach((input) => {
+    input.onchange = () => {
       myWorker.postMessage([first.value, second.value]);
-      console.log('Message posted to worker');
-    }
-  })
+      console.log("Message posted to worker");
+    };
+  });
 
-  myWorker.onmessage = function(e) {
+  myWorker.onmessage = (e) => {
     result.textContent = e.data;
-    console.log('Message received from worker');
-  }
+    console.log("Message received from worker");
+  };
 } else {
-  console.log('Your browser doesn\'t support web workers.');
+  console.log("Your browser doesn't support web workers.");
 }
