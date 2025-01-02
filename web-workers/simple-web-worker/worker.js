085b5952ccc3171b1075c4027868b191a745d3cf
Brian Smith <brian@smith.berlin>
2025-01-02 12:49:48 +0100

    chore: Format web workers dir (#288)

--- web-workers/simple-web-worker/worker.js
+++ web-workers/simple-web-worker/worker.js
@@ -1,11 +1,13 @@
-onmessage = function(e) {
-  console.log('Worker: Message received from main script');
+onmessage = (e) => {
+  console.log("Worker: Message received from main script");
+
   const result = e.data[0] * e.data[1];
+
   if (isNaN(result)) {
-    postMessage('Please write two numbers');
+    postMessage("Please write two numbers");
   } else {
-    const workerResult = 'Result: ' + result;
-    console.log('Worker: Posting message back to main script');
+    const workerResult = "Result: " + result;
+    console.log("Worker: Posting message back to main script");
     postMessage(workerResult);
   }
-}
+};
