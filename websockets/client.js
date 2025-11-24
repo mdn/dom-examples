aa2f11e5646fbf652a8f828781305af0da407c90
Kevin Farrugia <hello@imkev.dev>
2025-11-24 16:49:35 +0100

    Replace space with tabs

--- websockets/client.js
+++ websockets/client.js
@@ -5,39 +5,39 @@ let counter = 0;
 
 const logElement = document.querySelector("#log");
 function log(text) {
-  logElement.innerText = `${logElement.innerText}${text}\n`;
-  logElement.scrollTop = logElement.scrollHeight;
+	logElement.innerText = `${logElement.innerText}${text}\n`;
+	logElement.scrollTop = logElement.scrollHeight;
 }
 
 function initializeWebSocketListeners(ws) {
-  ws.addEventListener("open", () => {
-    log("CONNECTED");
-    pingInterval = setInterval(() => {
-      log(`SENT: ping: ${counter}`);
-      ws.send("ping");
-    }, 1000);
-  });
+	ws.addEventListener("open", () => {
+		log("CONNECTED");
+		pingInterval = setInterval(() => {
+			log(`SENT: ping: ${counter}`);
+			ws.send("ping");
+		}, 1000);
+	});
 
-  ws.addEventListener("close", () => {
-    log("DISCONNECTED");
-    clearInterval(pingInterval);
-  });
+	ws.addEventListener("close", () => {
+		log("DISCONNECTED");
+		clearInterval(pingInterval);
+	});
 
-  ws.addEventListener("message", (e) => {
-    log(`RECEIVED: ${e.data}: ${counter}`);
-    counter++;
-  });
+	ws.addEventListener("message", (e) => {
+		log(`RECEIVED: ${e.data}: ${counter}`);
+		counter++;
+	});
 
-  ws.addEventListener("error", (e) => {
-    log(`ERROR`);
-  });
+	ws.addEventListener("error", (e) => {
+		log(`ERROR`);
+	});
 }
 
 window.addEventListener("pageshow", (event) => {
-  if (event.persisted) {
-    websocket = new WebSocket(wsUri);
-    initializeWebSocketListeners(websocket);
-  }
+	if (event.persisted) {
+		websocket = new WebSocket(wsUri);
+		initializeWebSocketListeners(websocket);
+	}
 });
 
 log("OPENING");
@@ -46,10 +46,10 @@ initializeWebSocketListeners(websocket);
 
 // Close the websocket when the user leaves.
 window.addEventListener("pagehide", () => {
-  if (websocket) {
-    log("CLOSING");
-    websocket.close();
-    websocket = null;
-    window.clearInterval(pingInterval);
-  }
+	if (websocket) {
+		log("CLOSING");
+		websocket.close();
+		websocket = null;
+		window.clearInterval(pingInterval);
+	}
 });
