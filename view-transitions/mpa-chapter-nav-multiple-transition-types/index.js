e53f1a1137ac880495fa9834f745742f957dac99
Chris Mills <chrisdavidmills@gmail.com>
2025-12-01 13:59:00 +0000

    Add explanatory JS comments

--- view-transitions/mpa-chapter-nav-multiple-transition-types/index.js
+++ view-transitions/mpa-chapter-nav-multiple-transition-types/index.js
@@ -1,25 +1,36 @@
 const determineTransitionType = (oldNavigationEntry, newNavigationEntry) => {
+  // Grab the current and destination URLs from the provided navigation entries
   const currentURL = oldNavigationEntry.url;
   const destinationURL = newNavigationEntry.url;
 
+  // Find the index number of a page
   function determinePageIndex(url) {
+    // Split the URL at the "/" character
     const array = url.split("/");
+    // Grab the last array item, which should contain the end slug
     const slug = array[array.length - 1];
+    // If the URL doesn't include the HTML file name, return index 0
     if (slug.indexOf("html") === -1) {
       return 0;
     } else {
+      // Extract the number from the file name, e.g. index1.html -> 1
       const pageIndex = slug.replace("index", "").replace(".html", "");
+      // If it doesn't contain a number (i.e. index.html), return 0
       if (pageIndex === "") {
         return 0;
       } else {
+        // Return the number
         return parseInt(pageIndex);
       }
     }
   }
 
+  // Get the page index of the current and destination URLs
   const currentPageIndex = determinePageIndex(currentURL);
   const destinationPageIndex = determinePageIndex(destinationURL);
 
+  // Return "forwards" or "backwards" depending on whether currentPageIndex
+  // is bigger or smaller than destinationPageIndex
   if (currentPageIndex > destinationPageIndex) {
     return "backwards";
   }
@@ -29,20 +40,26 @@ const determineTransitionType = (oldNavigationEntry, newNavigationEntry) => {
 };
 
 window.addEventListener("pageswap", async (e) => {
+  // Grab the old and new navigation entries on the outgoing page from the pageswap event object's
+  // activation property, pass these to the determineTransitionType() function to determine the type
   const transitionType = determineTransitionType(
     e.activation.from,
     e.activation.entry
   );
   console.log(`pageSwap: ${transitionType}`);
+  // Add the type to the active ViewTransition via its types property
   e.viewTransition.types.add(transitionType);
 });
 
 window.addEventListener("pagereveal", async (e) => {
+  // Grab the old and new navigation entries on the incoming page from the Navigation.activation
+  // object, pass these to the determineTransitionType() function to determine the type
   const transitionType = determineTransitionType(
     navigation.activation.from,
     navigation.activation.entry
   );
 
   console.log(`pageReveal: ${transitionType}`);
+  // Add the type to the active ViewTransition via its types property
   e.viewTransition.types.add(transitionType);
 });
