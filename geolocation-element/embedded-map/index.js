50e21c0ea8dea76a9b34a750d1ccdce1546fbbd9
Chris Mills <chrisdavidmills@gmail.com>
2026-01-14 17:51:59 +0000

    Update map example

--- geolocation-element/embedded-map/index.js
+++ geolocation-element/embedded-map/index.js
@@ -1,31 +1,52 @@
-const geo = document.querySelector("geolocation");
-const fallback = document.querySelector("button");
 const statusElem = document.querySelector("p");
 
-// Test attributes:
-console.log(`isValid: ${geo.isValid}`);
-console.log(`invalidReason: ${geo.invalidReason}`);
-console.log(`autolocate: ${geo.autolocate}`);
-console.log(`watch: ${geo.watch}`);
-console.log(`initialPermissionStatus: ${geo.initialPermissionStatus}`);
-console.log(`permissionStatus: ${geo.permissionStatus}`);
+if (typeof HTMLGeolocationElement === "function") {
+  const geo = document.querySelector("geolocation");
 
-geo.addEventListener("location", () => {
-  if (geo.position) {
-    console.log(
-      `${geo.position.coords.latitude},${geo.position.coords.longitude}`
-    );
-    drawMap(geo.position.coords.latitude, geo.position.coords.longitude);
-  } else if (geo.error) {
-    console.log(geo.error.message);
+  // Test attributes:
+  console.log(`isValid: ${geo.isValid}`);
+  console.log(`invalidReason: ${geo.invalidReason}`);
+  console.log(`autolocate: ${geo.autolocate}`);
+  console.log(`watch: ${geo.watch}`);
+  console.log(`initialPermissionStatus: ${geo.initialPermissionStatus}`);
+  console.log(`permissionStatus: ${geo.permissionStatus}`);
+
+  geo.addEventListener("location", () => {
+    if (geo.position) {
+      console.log(
+        `${geo.position.coords.latitude},${geo.position.coords.longitude}`
+      );
+      drawMap(geo.position.coords.latitude, geo.position.coords.longitude, geo);
+    } else if (geo.error) {
+      console.log(geo.error.message);
+    }
+  });
+
+  geo.addEventListener("promptdismiss", notifyUserRetrySelection);
+  geo.addEventListener("promptaction", notifyUserGrantPermission);
+
+  function notifyUserRetrySelection() {
+    statusElem.textContent =
+      'Please press the "Use location" button again and allow location for this site.';
   }
-});
 
-geo.addEventListener("promptdismiss", notifyUserRetrySelection);
-geo.addEventListener("promptaction", notifyUserGrantPermission);
-// geo.addEventListener("validationstatuschange", notifyUserGrantPermission);
+  function notifyUserGrantPermission() {
+    if (geo.permissionStatus === "denied" || geo.permissionStatus === "prompt")
+      statusElem.textContent =
+        'Please press the "Use location" button and allow location for this site.';
+  }
+} else {
+  const fallback = document.querySelector("button");
 
-function drawMap(lat, long) {
+  // Fallback code
+  fallback.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      drawMap(position.coords.latitude, position.coords.longitude, fallback);
+    });
+  });
+}
+
+function drawMap(lat, long, btn) {
   const map = L.map("map").setView([lat, long], 13);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
@@ -35,23 +56,5 @@ function drawMap(lat, long) {
   const marker = L.marker([lat, long]).addTo(map);
 
   statusElem.textContent = "Map drawn successfully.";
-  geo.style.display = "none";
+  btn.style.display = "none";
 }
-
-function notifyUserRetrySelection() {
-  statusElem.textContent =
-    'Please press the "Use location" button again and allow location for this site.';
-}
-
-function notifyUserGrantPermission() {
-  if (geo.permissionStatus === "denied" || geo.permissionStatus === "prompt")
-    statusElem.textContent =
-      'Please press the "Use location" button and allow location for this site.';
-}
-
-// Fallback code
-fallback.addEventListener("click", () => {
-  navigator.geolocation.getCurrentPosition((position) => {
-    drawMap(position.coords.latitude, position.coords.longitude);
-  });
-});
