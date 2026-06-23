a5bd17f87a52ec1a00952e9826e7740268049cdd
Chris Mills <chrisdavidmills@gmail.com>
2026-06-23 09:27:32 +0100

    Add feature detection and code path for non-supporting browsers

--- scroll-promises/element-methods/index.js
+++ scroll-promises/element-methods/index.js
@@ -7,38 +7,63 @@ const toolbar = document.querySelector("div");
 const section = document.querySelector("section");
 const end = document.querySelector("#end");
 
-scrollBtn.addEventListener("click", async () => {
-  toolbar.className = "fade-out";
-  const result = await section.scroll(0, 1000);
-  console.log(
-    `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
-  );
-  toolbar.className = "fade-in";
-});
+function supportsScrollPromises() {
+  const test = section.scroll(0, 0);
+  return test instanceof Promise;
+}
 
-scrollToBtn.addEventListener("click", async () => {
-  toolbar.className = "fade-out";
-  const result = await section.scrollTo(0, 0);
-  console.log(
-    `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
-  );
-  toolbar.className = "fade-in";
-});
+const supports = supportsScrollPromises();
 
-scrollByBtn.addEventListener("click", async () => {
-  toolbar.className = "fade-out";
-  const result = await section.scrollBy(0, 200);
-  console.log(
-    `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
-  );
-  toolbar.className = "fade-in";
-});
+if (supports) {
+  scrollBtn.addEventListener("click", async () => {
+    toolbar.className = "fade-out";
+    const result = await section.scroll(0, 1000);
+    console.log(
+      `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
+    );
+    toolbar.className = "fade-in";
+  });
 
-scrollIntoViewBtn.addEventListener("click", async () => {
-  toolbar.className = "fade-out";
-  const result = await end.scrollIntoView();
-  console.log(
-    `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
-  );
-  toolbar.className = "fade-in";
-});
+  scrollToBtn.addEventListener("click", async () => {
+    toolbar.className = "fade-out";
+    const result = await section.scrollTo(0, 0);
+    console.log(
+      `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
+    );
+    toolbar.className = "fade-in";
+  });
+
+  scrollByBtn.addEventListener("click", async () => {
+    toolbar.className = "fade-out";
+    const result = await section.scrollBy(0, 200);
+    console.log(
+      `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
+    );
+    toolbar.className = "fade-in";
+  });
+
+  scrollIntoViewBtn.addEventListener("click", async () => {
+    toolbar.className = "fade-out";
+    const result = await end.scrollIntoView();
+    console.log(
+      `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
+    );
+    toolbar.className = "fade-in";
+  });
+} else {
+  scrollBtn.addEventListener("click", () => {
+    section.scroll(0, 1000);
+  });
+
+  scrollToBtn.addEventListener("click", () => {
+    section.scrollTo(0, 0);
+  });
+
+  scrollByBtn.addEventListener("click", () => {
+    section.scrollBy(0, 200);
+  });
+
+  scrollIntoViewBtn.addEventListener("click", () => {
+    end.scrollIntoView();
+  });
+}
--- scroll-promises/window-methods/index.js
+++ scroll-promises/window-methods/index.js
@@ -4,29 +4,50 @@ const scrollByBtn = document.querySelector(".scrollby");
 
 const toolbar = document.querySelector("div");
 
-scrollBtn.addEventListener("click", async () => {
-  toolbar.className = "fade-out";
-  const result = await window.scroll(0, 1000);
-  console.log(
-    `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
-  );
-  toolbar.className = "fade-in";
-});
+function supportsScrollPromises() {
+  const test = window.scroll(0, 0);
+  return test instanceof Promise;
+}
 
-scrollToBtn.addEventListener("click", async () => {
-  toolbar.className = "fade-out";
-  const result = await window.scrollTo(0, 0);
-  console.log(
-    `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
-  );
-  toolbar.className = "fade-in";
-});
+const supports = supportsScrollPromises();
 
-scrollByBtn.addEventListener("click", async () => {
-  toolbar.className = "fade-out";
-  const result = await window.scrollBy(0, 200);
-  console.log(
-    `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
-  );
-  toolbar.className = "fade-in";
-});
+if (supports) {
+  scrollBtn.addEventListener("click", async () => {
+    toolbar.className = "fade-out";
+    const result = await window.scroll(0, 1000);
+    console.log(
+      `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
+    );
+    toolbar.className = "fade-in";
+  });
+
+  scrollToBtn.addEventListener("click", async () => {
+    toolbar.className = "fade-out";
+    const result = await window.scrollTo(0, 0);
+    console.log(
+      `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
+    );
+    toolbar.className = "fade-in";
+  });
+
+  scrollByBtn.addEventListener("click", async () => {
+    toolbar.className = "fade-out";
+    const result = await window.scrollBy(0, 200);
+    console.log(
+      `Scroll finished;${result.interrupted ? " " : " not "}interrupted`,
+    );
+    toolbar.className = "fade-in";
+  });
+} else {
+  scrollBtn.addEventListener("click", () => {
+    window.scroll(0, 1000);
+  });
+
+  scrollToBtn.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
+
+  scrollByBtn.addEventListener("click", () => {
+    window.scrollBy(0, 200);
+  });
+}
